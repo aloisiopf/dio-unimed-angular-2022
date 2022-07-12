9de7c3efb00259282d49c3108719c7f84f84af4e
aloisiopf <aloisiopf@gmail.com>
2022-07-12 09:07:21 -0300

    Criação do componente User-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: UsersListComponent }
+  { path: '', component: UsersListComponent },
+  { path: 'form', component: UserFormComponent }
 ];
 
 @NgModule({
